27fc26b7a3e314521bbce5926eedf36f110838c4
Cedric Boucher <onebitalpha@gmail.com>
2023-08-12 12:47:24 -0600

    further testing of regex

--- regex_testing.py
+++ regex_testing.py
@@ -1,7 +1,11 @@
 import re
+import os
 
-string = "hello"
+p = re.compile(".*\\.py$") # all files that end in ".py"
 
-p = re.compile("hel*")
-
-print(p.match(string).group())
+for folderpath, _, filenames in os.walk("C:/Users/onebi/Documents/GitHub/Mass-Video-Transcoder"):
+    filepaths = tuple([os.path.abspath(folderpath+"/"+filename) for filename in filenames])
+    for filepath in filepaths:
+        match = p.match(filepath)
+        if match:
+            print(filepath)
