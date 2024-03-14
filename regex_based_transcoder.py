e26cd43f9063c347d64e7b3af05a8042a82a9249
Cedric Boucher <onebitalpha@gmail.com>
2024-03-13 19:51:43 -0600

    fixed type hints

--- regex_based_transcoder.py
+++ regex_based_transcoder.py
@@ -8,25 +8,40 @@ from send2trash import send2trash
 
 DRY_RUN = False # if True, runs without affecting any files
 
-filepath_match_pairs: list[tuple[re.Pattern, dict, str]] = [
-    (
-        "C:\\\\Users\\\\onebi\\\\Documents\\\\GitHub\\\\Mass-Video-Transcoder\\\\regex_testing_folder.*\\.mp4$",
-        {"vcodec": "libsvtav1", "c:a": "libopus", "b:a": "128K", "g": "600", "vf": "scale=out_range=full", "svtav1-params": "preset=12:crf=63:matrix-coefficients=bt709:color-range=1:color-primaries=bt709"},
+filepath_match_pairs: list[tuple[str, dict[str, str], str]] = [
+    #(
+    #    "C:\\\\Users\\\\onebi\\\\Documents\\\\GitHub\\\\Mass-Video-Transcoder\\\\regex_testing_folder.*\\.mp4$",
+    #    {"vcodec": "libsvtav1", "c:a": "libopus", "b:a": "128K", "g": "600", "vf": "scale=out_range=full", "svtav1-params": "preset=12:crf=63:matrix-coefficients=bt709:color-range=1:color-primaries=bt709"},
+    #    ".webm"
+    #),
+    #(
+    #    "K:\\\\Unbacked up\\\\Screen Recordings.*\\\\Celeste.*\\.mkv$",
+    #    {"vcodec": "libsvtav1", "c:a": "libopus", "b:a": "256K", "g": "600", "vf": "scale=out_range=full", "svtav1-params": "preset=5:crf=30:matrix-coefficients=bt709:color-range=1:color-primaries=bt709"},
+    #    ".webm"
+    #),
+    #(
+    #    "K:\\\\Unbacked up\\\\Screen Recordings.*\\\\The Entropy Centre.*\\.mkv$",
+    #    {"vcodec": "libsvtav1", "c:a": "libopus", "b:a": "256K", "g": "600", "vf": "scale=out_range=full", "svtav1-params": "preset=5:crf=30:matrix-coefficients=bt709:color-range=1:color-primaries=bt709"},
+    #    ".webm"
+    #),
+    #(
+    #    "K:\\\\Unbacked up\\\\Screen Recordings.*\\\\Mini Motorways.*\\.mkv$",
+    #    {"vcodec": "libsvtav1", "c:a": "libopus", "b:a": "256K", "g": "600", "vf": "scale=out_range=full", "svtav1-params": "preset=5:crf=30:matrix-coefficients=bt709:color-range=1:color-primaries=bt709"},
+    #    ".webm"
+    #),
+    ( # OP6T videos
+        "K:\\\\Unbacked up\\\\Screen Recordings.*\\\\mass transcoder\\\\VID_[0-9]*_[0-9]*.*\\.mp4$",
+        {"vcodec": "libsvtav1", "c:a": "libopus", "b:a": "192K", "g": "600", "vf": "scale=out_range=full", "svtav1-params": "preset=3:crf=20:matrix-coefficients=bt709:color-range=1:color-primaries=bt709"},
         ".webm"
     ),
-    (
-        "K:\\\\Unbacked up\\\\Screen Recordings.*\\\\Celeste.*\\.mkv$",
-        {"vcodec": "libsvtav1", "c:a": "libopus", "b:a": "256K", "g": "600", "vf": "scale=out_range=full", "svtav1-params": "preset=6:crf=30:matrix-coefficients=bt709:color-range=1:color-primaries=bt709"},
+    ( # OP6T videos
+        "K:\\\\Unbacked up\\\\Screen Recordings.*\\\\mass transcoder\\\\OP6T_VID_[0-9]*_[0-9]*.*\\.mp4$",
+        {"vcodec": "libsvtav1", "c:a": "libopus", "b:a": "192K", "g": "600", "vf": "scale=out_range=full", "svtav1-params": "preset=3:crf=20:matrix-coefficients=bt709:color-range=1:color-primaries=bt709"},
         ".webm"
     ),
-    (
-        "K:\\\\Unbacked up\\\\Screen Recordings.*\\\\The Entropy Centre.*\\.mkv$",
-        {"vcodec": "libsvtav1", "c:a": "libopus", "b:a": "256K", "g": "600", "vf": "scale=out_range=full", "svtav1-params": "preset=6:crf=30:matrix-coefficients=bt709:color-range=1:color-primaries=bt709"},
-        ".webm"
-    ),
-    (
-        "K:\\\\Unbacked up\\\\Screen Recordings.*\\\\Mini Motorways.*\\.mkv$",
-        {"vcodec": "libsvtav1", "c:a": "libopus", "b:a": "256K", "g": "600", "vf": "scale=out_range=full", "svtav1-params": "preset=6:crf=30:matrix-coefficients=bt709:color-range=1:color-primaries=bt709"},
+    ( # phone screen recordings
+        "K:\\\\Unbacked up\\\\Screen Recordings.*\\\\mass transcoder\\\\[0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9].*\\.mp4$",
+        {"vcodec": "libsvtav1", "c:a": "libopus", "b:a": "192K", "g": "600", "vf": "scale=out_range=full", "svtav1-params": "preset=3:crf=30:matrix-coefficients=bt709:color-range=1:color-primaries=bt709"},
         ".webm"
     )
 ]
@@ -34,17 +49,18 @@ filepath_match_pairs: list[tuple[re.Pattern, dict, str]] = [
 # any one file will run the first command that it gets matched to with regex, even if multiples pairs would have matched
 # it is expected that your output file specification will not be overwriting your input file specification
 
-operations: list[str] = list() # list of complete FFMPEG commands
+operations: list[dict[str, str]] = list() # list of complete FFMPEG commands
 
 # convert regex strings to re.Pattern objects
+filepath_match_pairs_processed: list[tuple[re.Pattern, dict[str, str], str]] = []
 i = -1
 for match_pair in filepath_match_pairs:
     i += 1
-    match_pair = list(match_pair) # convert to list to make it mutable
-    match_pair[0] = re.compile(match_pair[0])
-    filepath_match_pairs[i] = match_pair # overwrite the tuple that was there since tuples are immutable
+    re_compiled: re.Pattern = re.compile(match_pair[0])
+    match_pair_re: tuple[re.Pattern, dict, str] = (re_compiled, match_pair[1], match_pair[2])
+    filepath_match_pairs_processed[i] = match_pair_re # overwrite the tuple that was there since tuples are immutable
 
-paths_to_search: tuple[str] = (
+paths_to_search: tuple[str, ...] = (
     "C:\\Users\\onebi\\Documents\\GitHub\\Mass-Video-Transcoder\\regex_testing_folder",
     "K:\\Unbacked up\\Screen Recordings"
 )
@@ -60,15 +76,18 @@ async def run_ffmpeg(ffmpeg: FFmpeg):
     await ffmpeg.execute()
     print("\n")
 
+total_size_original = 0
+total_size_output = 0
 
 for path_to_search in paths_to_search:
     for folderpath, _, filenames in os.walk(path_to_search):
         filepaths = tuple([os.path.abspath(folderpath+"/"+filename) for filename in filenames]) # convert to absolute filepaths
         for filepath in filepaths:
-            for match_pair in filepath_match_pairs:
+            for match_pair in filepath_match_pairs_processed:
                 # try to match filepath with each regex expression until there's a match (or we've exhausted the list)
                 match = match_pair[0].match(filepath)
                 if match:
+                    print("\n\n--------------------------------------------------")
                     # get full output filepath and remove file extension from filename as it is determined by transcode
                     output_folderpath, output_filename = os.path.split(filepath)
                     period_count = output_filename.count(".")
@@ -91,14 +110,17 @@ for path_to_search in paths_to_search:
                     ffmpeg = FFmpeg()
                     ffmpeg.option("y")
                     ffmpeg.input(filepath)
-                    ffmpeg.output(output_filepath_in_progress, operation)
+                    ffmpeg.output(output_filepath_in_progress, operation) # type: ignore
                     if not DRY_RUN:
                         try:
                             asyncio.run(run_ffmpeg(ffmpeg))
                             size_original: int = os.path.getsize(filepath)
                             size_output: int = os.path.getsize(output_filepath_in_progress)
-                            print("Original Size: {:.0f} MB\nOutput Size:   {:.0f} MB\n"
-                                  .format(size_original/1000000, size_output/1000000))
+                            size_fraction: float = size_output / size_original * 100
+                            print("Original Size: {:.0f} MB\nOutput Size:   {:.0f} MB\nPercent:       {:.2f}%\n"
+                                  .format(size_original/1000000, size_output/1000000, size_fraction))
+                            total_size_original += size_original
+                            total_size_output += size_output
                             os.rename(output_filepath_in_progress, output_filepath) # remove "_in_progress" once file is done being created
                         except FFmpegError:
                             # transcode had at least one error, aborting
@@ -110,8 +132,12 @@ for path_to_search in paths_to_search:
                             os.rename(output_filepath_in_progress, output_filepath)
 
 
-                    send2trash(filepath) # delete original once transcode has successfully completed
+                    #send2trash(filepath) # delete original once transcode has successfully completed
 
+                    print("--------------------------------------------------\n")
                     break # don't compare this file to any more regex
 
 print("files transcoded: {}".format(len(operations)))
+total_size_fraction: float = total_size_output / total_size_original * 100
+print("\nOriginal Size: {:.0f} MB\nOutput Size:   {:.0f} MB\nPercent:       {:.2f}%"
+      .format(total_size_original/1000000, total_size_output/1000000, total_size_fraction))
